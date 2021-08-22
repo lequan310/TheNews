5f730dfb1f13fd067d3df9ba3488d911f0cc99b5
Yukiri <leminhquan310@gmail.com>
2021-08-22 21:58:12 +0700

    Push for ashton

--- src/main/ArticleController.java
+++ src/main/ArticleController.java
@@ -530,7 +530,7 @@ public class ArticleController implements Initializable{
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Poor Internet Connection");
         alert.setHeaderText("Can't connect to\n" + item.getLink());
-        alert.setContentText("Please check your internet connection.");
+        alert.setContentText("Please check your internet connection. Press F5 to refresh article.");
 
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
