056f68a44fe628f26a3419b0291bdbabbd907dab
Yukiri <leminhquan310@gmail.com>
2021-08-24 15:25:24 +0700

    Change libraries

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
         stage.setTitle("News Application");
         stage.getIcons().add(new Image("image/book.jpg"));
         stage.setScene(scene);
+        stage.setResizable(true);
         stage.setMinHeight(800);
         stage.setMinWidth(600);
         stage.initStyle(StageStyle.UNDECORATED);
